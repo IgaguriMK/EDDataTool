ae3a492c82a22fc991e7bd2c8785d88728c4e8af
Igaguri <igagurimk@gmail.com>
2017-12-21 10:36:24 +0900

    [WIP]

--- jcheck.go
+++ jcheck.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"time"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	args := os.Args
+	if len(args) < 2 {
+		log.Fatal("Too few arguments.")
+	}
+
+	args = args[1:]
+
+	for _, fname := range args {
+		checkIn(fname)
+	}
+}
+
+func checkIn(fname string) {
+	file, err := os.Open(fname)
+	if err != nil {
+		log.Printf("Warning: Cannot open '%s'.", fname)
+		return
+	}
+	defer file.Close()
+
+	sc := bufio.NewScanner(file)
+
+	for sc.Scan() {
+		eventStr := sc.Text()
+		fmt.Println(eventStr)
+	}
+}
+
+type anyEvent struct {
+	Event     string    `json:"event"`
+	Timestamp time.Time `json:"timestamp"`
 }
